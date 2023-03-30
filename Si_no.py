c2381bb3f30c0be7ec4399a07b41193e39a8ecde
Csar3 <91861850+Csar3@users.noreply.github.com>
2023-03-29 23:15:53 -0600

    Add files via upload

---
+++ Si_no.py
@@ -0,0 +1,14 @@
+num1 = float(input("Numero 1: "))
+num2 = float(input("Numero 2: "))
+
+a= float(num1+num2)
+b= float(a/2)
+
+print(b)
+
+if b >= 60:
+    print("Aprueba")
+
+if b <= 60:
+    print("Reprueba")
+
