9a7aadf2965ee63b7b8c3c17d66c9958b9acdc75
Csar3 <91861850+Csar3@users.noreply.github.com>
2023-03-31 00:03:20 -0600

    Add files via upload

---
+++ calcu.py
@@ -0,0 +1,12 @@
+radio = float(input("Area: "))
+altura = float(input("Altura: "))
+
+pi = float(3.14)
+
+volumen = pi*radio**2*altura
+
+print(volumen,"cm")
+
+cond = volumen >=300
+print(cond)
+    
